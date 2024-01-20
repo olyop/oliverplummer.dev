84054a4de18f2b058692d5acb49516c7f1846064
Oliver Plummer <oliver.plummer@outlook.com>
2024-01-20 17:33:38 +1100

    Update

--- client/src/layout/header/contact/get-contact-details.ts
+++ client/src/layout/header/contact/get-contact-details.ts
@@ -2,11 +2,14 @@ export const getContactDetails = async (reCaptchaToken: string) => {
 	const url = new URL(import.meta.env.VITE_GET_CONTACT_DETAILS_URL);
 	url.searchParams.set("reCaptchaToken", reCaptchaToken);
 
-	return fetch(url, {
+	const headers = new Headers();
+	headers.append("Accept", "application/json");
+
+	const request = new Request(url, {
 		method: "GET",
 		mode: "cors",
-		headers: {
-			"Accept": "application/json",
-		},
+		headers,
 	});
+
+	return fetch(request);
 };
