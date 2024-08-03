63fd769eb0f2c854f31bd054b55ee8349bbb42d8
Oliver Plummer <oliver.plummer@outlook.com>
2024-08-03 18:20:53 +1000

    sync

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,41 +2,23 @@ import { readFile } from "node:fs/promises";
 
 import reactSwc from "@vitejs/plugin-react-swc";
 import contentSecurityPolicyBuilder from "content-security-policy-builder";
-import { Plugin, defineConfig } from "vite";
+import { defineConfig } from "vite";
 import { imagetools } from "vite-imagetools";
+import { createHtmlPlugin } from "vite-plugin-html";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const html = (variables: Record<string, string>): Plugin => ({
-	name: "html-transform",
-	transformIndexHtml: {
-		order: "pre",
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-		handler: (value: string): string =>
-			value.replaceAll(/{{ (.*?) }}/g, (match, p1) => variables[p1] ?? match),
-	},
-});
-
-const determineContentSecurityPolicy = (mode: string) => {
-	const isProduction = mode === "production";
-
-	return contentSecurityPolicyBuilder({
-		directives: {
-			"default-src": ["'self'"],
-			"script-src": ["'self'", isProduction ? "" : "'unsafe-inline'"],
-			"style-src": ["'self'", "'unsafe-inline'"],
-			"font-src": ["'self'"],
-			"connect-src": ["'self'"],
-			"img-src": ["'self'"],
-			"media-src": ["'self'"],
-		},
-	});
-};
-
-export default defineConfig(async ({ mode }) => ({
+const configuration = defineConfig(async ({ mode }) => ({
 	plugins: [
 		reactSwc(),
 		tsconfigPaths(),
-		html({ "VITE_CSP": determineContentSecurityPolicy(mode) }),
+		createHtmlPlugin({
+			minify: true,
+			inject: {
+				data: {
+					contentSecurityPolicy: determineContentSecurityPolicy(mode),
+				},
+			},
+		}),
 		imagetools(),
 	],
 	server: {
@@ -47,3 +29,21 @@ export default defineConfig(async ({ mode }) => ({
 		},
 	},
 }));
+
+function determineContentSecurityPolicy(mode: string) {
+	const isProduction = mode === "production";
+
+	return contentSecurityPolicyBuilder({
+		directives: {
+			"default-src": ["'self'"],
+			"script-src": ["'self'", isProduction ? "" : "'unsafe-inline'"],
+			"style-src": ["'self'", "'unsafe-inline'"],
+			"font-src": ["'self'"],
+			"connect-src": ["'self'"],
+			"img-src": ["'self'", "data:"],
+			"media-src": ["'self'"],
+		},
+	});
+}
+
+export default configuration;
